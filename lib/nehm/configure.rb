02068705c03460b0e3bfbfa84dfdb5cf539493a5
Albert <albertnigma@gmail.com>
2015-07-31 11:09:47 +0500

    Edit looped configure menu

--- lib/nehm/configure.rb
+++ lib/nehm/configure.rb
@@ -1,12 +1,12 @@
 # Configure module responds to 'nehm configure' command
 module Configure
   def self.menu
-    puts 'Download path: ' + Paint[PathControl.dl_path, :magenta] if PathControl.dl_path
-    puts 'iTunes path: ' + Paint[PathControl.itunes_path_name, :magenta] if PathControl.itunes_path
-    puts 'Permalink: ' + Paint[Config[:permalink], :cyan] if Config[:permalink]
-    puts "\n"
-
     loop do
+      puts 'Download path: ' + Paint[PathControl.dl_path, :magenta] if PathControl.dl_path
+      puts 'iTunes path: ' + Paint[PathControl.itunes_path_name, :magenta] if PathControl.itunes_path
+      puts 'Permalink: ' + Paint[Config[:permalink], :cyan] if Config[:permalink]
+      puts "\n"
+
       HighLine.new.choose do |menu|
         menu.prompt = Paint['Choose setting', :yellow]
 
