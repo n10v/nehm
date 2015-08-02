e3413d4769291d266e879fc2b10db892dea9b82f
Albert <albertnigma@gmail.com>
2015-08-02 23:30:22 +0500

    Freeze some strings

--- lib/nehm/configure.rb
+++ lib/nehm/configure.rb
@@ -10,10 +10,10 @@ module Configure
       HighLine.new.choose do |menu|
         menu.prompt = Paint['Choose setting', :yellow]
 
-        menu.choice('Edit download path') { PathControl.set_dl_path }
-        menu.choice('Edit itunes path') { PathControl.set_itunes_path } unless OS.linux?
-        menu.choice('Edit permalink') { UserControl.log_in }
-        menu.choice('Exit') { exit }
+        menu.choice('Edit download path'.freeze) { PathControl.set_dl_path }
+        menu.choice('Edit itunes path'.freeze) { PathControl.set_itunes_path } unless OS.linux?
+        menu.choice('Edit permalink'.freeze) { UserControl.log_in }
+        menu.choice('Exit'.freeze) { exit }
       end
       sleep(1)
       puts "\n"
--- lib/nehm/version.rb
+++ lib/nehm/version.rb
@@ -1,3 +1,3 @@
 module Nehm
-  VERSION = '1.3.2'
+  VERSION = '1.3.2'.freeze
 end
