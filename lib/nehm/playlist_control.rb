f9c6b4555c986d9a5f7e7058ff186c8c1f5bdb9b
Albert <albertnigma@gmail.com>
2015-08-08 13:03:36 +0500

    Add modules and scripts, working with iTunes playlists

---
+++ lib/nehm/playlist_control.rb
@@ -0,0 +1,20 @@
+require 'nehm/applescripts'
+module PlaylistControl
+  def self.playlist
+    @temp_playlist || Config[:playlist]
+  end
+
+  def self.set_playlist
+    loop do
+      playlist = Highline.new.ask('Enter name of default iTunes playlist to which you want add tracks')
+
+      if AppleScripts.list_of_playlists.include? playlist
+        Config[:playlist] = playlist
+        puts Paint["Default iTunes playlist set up to #{playlist}", :green]
+        break
+      else
+        puts Paint['Invalid playlist name. Please enter correct playlist name', :red]
+      end
+    end
+  end
+end
