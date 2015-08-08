b5517d6d20211d7e11c2ee15cf1238cebf65c635
Albert <albertnigma@gmail.com>
2015-08-08 17:33:38 +0500

    Edit dependencies

--- lib/nehm/playlist.rb
+++ lib/nehm/playlist.rb
@@ -1,4 +1,3 @@
-require 'nehm/applescripts'
 class Playlist
   attr_reader :name
 
@@ -7,6 +6,7 @@ class Playlist
   end
 
   def add_track(track_path)
+    puts 'Adding to playlist'
     AppleScripts.add_track_to_playlist(track_path, @name)
   end
 end
