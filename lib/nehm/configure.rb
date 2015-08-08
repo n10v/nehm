01928575b98934bbfa1e1a2b19d4e50d4fc71682
Albert <albertnigma@gmail.com>
2015-08-08 14:38:59 +0500

    Add check for unset up options

--- lib/nehm/configure.rb
+++ lib/nehm/configure.rb
@@ -2,20 +2,59 @@
 module Configure
   def self.menu
     loop do
-      puts 'Download path: ' + Paint[PathControl.dl_path, :magenta] if PathControl.dl_path
-      puts 'iTunes path: ' + Paint[PathControl.itunes_path_name, :magenta] if PathControl.itunes_path
-      puts 'Permalink: ' + Paint[Config[:permalink], :cyan] if Config[:permalink]
-      puts "\n"
+      output = ''
+
+      # Download path
+      output << 'Download path: '
+      output <<
+        if Config[:dl_path]
+          Paint[Config[:dl_path], :magenta]
+        else
+          Paint["doesn't set up", :gold]
+        end
+      output << "\n"
+
+      # iTunes path
+      output << 'iTunes path: '
+      output <<
+        if PathControl.itunes_path
+          Paint[PathControl.itunes_path_name, :magenta]
+        else
+          Paint["doesn't set up", :gold]
+        end
+      output << "\n"
+
+      # Permalink
+      output << 'Permalink: '
+      output <<
+        if Config[:permalink]
+          Paint[Config[:permalink], :cyan]
+        else
+          Paint["doesn't set up", :gold]
+        end
+      output << "\n"
+
+      # Playlist
+      output << 'Playlist: '
+      output <<
+        if PlaylistControl.playlist
+          Paint[PlaylistControl.playlist, :cyan]
+        else
+          Paint["doesn't set up", :gold]
+        end
+      output << "\n"
+
+      puts output
 
       HighLine.new.choose do |menu|
         menu.prompt = Paint['Choose setting', :yellow]
 
         menu.choice('Edit download path'.freeze) { PathControl.set_dl_path }
-        menu.choice('Edit itunes path'.freeze) { PathControl.set_itunes_path } unless OS.linux?
+        menu.choice('Edit iTunes path'.freeze) { PathControl.set_itunes_path } unless OS.linux?
         menu.choice('Edit permalink'.freeze) { UserControl.log_in }
+        menu.choice('Edit iTunes playlist'.freeze) { PlaylistControl.set_playlist } unless OS.linux?
         menu.choice('Exit'.freeze) { exit }
       end
-      sleep(1)
       puts "\n"
     end
   end
