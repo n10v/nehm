5776562d4b038349d6a497ee57bd9fd0e6c95196
Albert <albertnigma@gmail.com>
2015-08-29 13:38:59 +0500

    Fix problems with special symbols

--- lib/nehm/applescript.rb
+++ lib/nehm/applescript.rb
@@ -1,15 +1,15 @@
 module AppleScript
   def self.add_track_to_playlist(track_path, playlist_name)
-    `osascript #{script_path(:add_track_to_playlist)} #{track_path} #{playlist_name} > /dev/null`
+    `osascript \"#{script_path(:add_track_to_playlist)}\" \"#{track_path}\" \"#{playlist_name}\" > /dev/null`
   end
 
   def self.list_of_playlists
-    output = `osascript #{script_path(:list_of_playlists)}`
+    output = `osascript \"#{script_path(:list_of_playlists)}\"`
     output.chomp.split(', ')
   end
 
   def self.music_master_library
-    `osascript #{script_path(:music_master_library)}`
+    `osascript \"#{script_path(:music_master_library)}\"`
   end
 
   module_function
