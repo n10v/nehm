a9d1ef4fdad0ff8252454a50e76b5fc93aab449a
Albert <albertnigma@gmail.com>
2015-09-12 22:37:52 +0500

    Add check for OS in PlaylistManager.playlist

--- lib/nehm/playlist_manager.rb
+++ lib/nehm/playlist_manager.rb
@@ -1,14 +1,14 @@
 module Nehm
   module PlaylistManager
     def self.playlist
-      @temp_playlist || default_user_playlist || music_master_library
+      @temp_playlist || default_user_playlist || music_master_library unless OS.linux?
     end
 
     def self.set_playlist
       loop do
         playlist = HighLine.new.ask('Enter name of default iTunes playlist to which you want add tracks (press Enter to set it to default iTunes Music library)')
 
-        # If entered nothing, unset iTunes playlist 
+        # If entered nothing, unset iTunes playlist
         if playlist == ''
           Cfg[:playlist] = nil
           puts Paint['Default iTunes playlist unset', :green]
