57466551c22815e0b0f7cd1450a839b2eb659e95
Albert <albertnigma@gmail.com>
2015-09-26 13:20:42 +0500

    Edit namespace

--- lib/nehm.rb
+++ lib/nehm.rb
@@ -18,52 +18,50 @@ require 'nehm/user_manager'
 require 'nehm/version'
 
 module Nehm
-  module App
-    def self.do(args)
-      init unless initialized?
+  def self.do(args)
+    init unless initialized?
 
-      command = args.shift
-      case command
-      when 'get'
-        Get[:get, args]
-      when 'dl'
-        Get[:dl, args]
-      when 'configure'
-        Configure.menu
-      when 'version'
-        puts Nehm::VERSION
-      when 'help', nil
-        Help.show(args.first)
-      else
-        puts "Invalid command '#{command}'".red
-        puts "Input #{'nehm help'.yellow} for all avalaible commands"
-      end
+    command = args.shift
+    case command
+    when 'get'
+      Get[:get, args]
+    when 'dl'
+      Get[:dl, args]
+    when 'configure'
+      Configure.menu
+    when 'version'
+      puts Nehm::VERSION
+    when 'help', nil
+      Help.show(args.first)
+    else
+      puts "Invalid command '#{command}'".red
+      puts "Input #{'nehm help'.yellow} for all avalaible commands"
     end
+  end
 
-    module_function
+  module_function
 
-    def init
-      puts 'Hello!'.green
-      puts 'Before using the nehm, you should set it up:'
-      Cfg.create unless Cfg.exist?
+  def init
+    puts 'Hello!'.green
+    puts 'Before using the nehm, you should set it up:'
+    Cfg.create unless Cfg.exist?
 
-      PathManager.set_dl_path
+    PathManager.set_dl_path
+    puts "\n"
+
+    unless OS.linux?
+      PlaylistManager.set_playlist
       puts "\n"
-
-      unless OS.linux?
-        PlaylistManager.set_playlist
-        puts "\n"
-      end
-
-      UserManager.log_in
-      puts "\n"
-
-      puts "Now you can use nehm!\n".green
-      sleep(1)
     end
 
-    def initialized?
-      File.exist?(Cfg::FILE_PATH)
-    end
+    UserManager.log_in
+    puts "\n"
+
+    puts "Now you can use nehm!\n".green
+    sleep(1)
+  end
+
+  def initialized?
+    File.exist?(Cfg::FILE_PATH)
   end
 end
