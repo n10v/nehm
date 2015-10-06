0b5c54e0ae17dd38dbca96f8c0bdd620de6a118c
bogem <albertnigma@gmail.com>
2015-10-06 22:09:02 +0200

    Add all logic to 'version' command

--- lib/nehm/commands/version_command.rb
+++ lib/nehm/commands/version_command.rb
@@ -1,4 +1,21 @@
 module Nehm
-  class DlCommand < Command
+  class VersionCommand < Command
+
+    def execute
+      UI.say VERSION
+    end
+
+    def program_name
+      'nehm version'
+    end
+
+    def summary
+      "Show nehm's verion"
+    end
+
+    def usage
+      program_name
+    end
+
   end
 end
