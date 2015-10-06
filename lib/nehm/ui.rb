8da2019c955d708051a8b87d13457e7efa7623ba
bogem <albertnigma@gmail.com>
2015-10-06 21:32:54 +0200

    Use msg instead of message in UI module

--- lib/nehm/ui.rb
+++ lib/nehm/ui.rb
@@ -1,31 +1,31 @@
 module Nehm
   module UI
-    def self.error(message)
-      puts "#{message}\n".red
+    def self.error(msg)
+      puts "#{msg}\n".red
     end
 
     def self.newline
       puts "\n"
     end
 
-    def self.say(message)
-      puts message
+    def self.say(msg)
+      puts msg
     end
 
-    def self.success(message)
-      puts message.green
+    def self.success(msg)
+      puts msg.green
     end
 
-    def self.term(message = nil)
-      if message
-        abort message.red
+    def self.term(msg = nil)
+      if msg
+        abort msg.red
       else
         exit
       end
     end
 
-    def self.warning
-      puts "#{message}\n".yellow
+    def self.warning(msg)
+      puts "#{msg}\n".yellow
     end
 
   end
