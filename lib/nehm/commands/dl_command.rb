38d32ba8d5f6664b8b9055c8c1750ce1f764daea
bogem <albertnigma@gmail.com>
2015-10-06 16:54:01 +0200

    Add 'dl' and 'get' commands

--- lib/nehm/commands/dl_command.rb
+++ lib/nehm/commands/dl_command.rb
@@ -1,4 +1,41 @@
+require 'nehm/tracks'
+
 module Nehm
   class DlCommand < Command
+
+    def initialize
+      super
+
+      add_option(:from, 'from PERMALINK',
+                 'Get track(s) from user with PERMALINK')
+
+      add_option(:to, 'to PATH',
+                 'Download track(s) to PATH')
+    end
+
+    def execute
+      Tracks[:dl, @options]
+    end
+
+    def arguments
+      [['post'.green,           'Download last post (track or repost) from your profile'],
+       ['<number> posts'.green, 'Download last <number> posts from your profile'],
+       ['like'.green,           'Download your last like'],
+       ['<number> likes'.green, 'Download your last <number> likes'],
+       ['url'.magenta,          'Download track from entered url']]
+    end
+
+    def program_name
+      'nehm dl'
+    end
+
+    def summary
+      'Download and set tags any track from Soundcloud'
+    end
+
+    def usage
+      "#{program_name} ARGUMENT [OPTIONS]"
+    end
+
   end
 end
